42eb3d63ccfb6a43cd4537ae416705aa9a20fb90
Andreas Loo <andreas.loo@smhi.se>
2019-10-16 09:55:59 +0200

    Add webpack config file

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+const path = require('path');
+
+module.exports = {
+  entry: path.resolve(__dirname, 'src', 'index.js'),
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, 'dist'),
+    host: '0.0.0.0'
+  }
+};
