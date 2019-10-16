5b3e955c7687623ae87a73c5deb5b1bfb4cb7d26
Andreas Loo <andreas.loo@smhi.se>
2019-10-16 10:44:40 +0200

    Test communication with backend

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 console.log('This is the nµa frontend project!');
+
+fetch('/api/').then(response => {
+  response.json().then(data => console.log(
+    `Recieved from backend: ${data.text}`
+  ));
+});
