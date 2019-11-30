0044962426917f8be05dc6720ac90c26cb5865c9
Andreas Loo <andreas.loo@smhi.se>
2019-11-30 13:29:17 +0100

    Add first draft of Logo component

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+const Logo = () => (
+  <svg viewBox="0 0 80 110" width="80" height="110">
+    <path d="M 0 110 L 0 0 L 20 0 L 20 70 L 60 70 L 60 0 L 80 0 L 80 80 L 20 80 L 20 110 L 0 110 Z" fill="#000" />
+  </svg>
+);
+
+export default Logo;
