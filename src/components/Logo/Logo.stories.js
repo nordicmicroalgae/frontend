348c385a1791917a8264c3a08089014e16e4011c
Andreas Loo <andreas.loo@smhi.se>
2019-11-30 15:36:03 +0100

    Update Logo stories

--- src/components/Logo/Logo.stories.js
+++ src/components/Logo/Logo.stories.js
@@ -3,6 +3,24 @@ import Logo from './';
 
 export default { title: 'Logo' };
 
-export const basic = () => (
-  <Logo />
+export const size16 = () => (
+  <Logo size={16} />
+);
+
+export const size32 = () => (
+  <Logo size={32} />
+);
+
+export const size64 = () => (
+  <Logo size={64} />
+);
+
+export const size128 = () => (
+  <Logo size={128} />
+);
+
+export const lightTheme = () => (
+  <div style={{width: '128px', padding: '2rem', background: '#000', textAlign: 'center'}}>
+    <Logo size={128} theme="light" />
+  </div>
 );
