54ba06fd139f642ff7cea114024696bfc1fa5236
Andreas Loo <andreas.loo@smhi.se>
2019-11-30 15:28:12 +0100

    Replace color configuration with theme (light/dark) for Logo

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -3,18 +3,18 @@ import PropTypes from 'prop-types';
 
 
 const propTypes = {
-  color: PropTypes.string,
+  theme: PropTypes.oneOf(['dark', 'light']),
   size: PropTypes.oneOf([16, 32, 64, 128])
 };
 
 const defaultProps = {
-  color: '#000',
+  theme: 'dark',
   size: 32
 };
 
-const Logo = ({ color, size }) => (
-  <svg viewBox="0 0 11 16" width={Math.round(size * 11 / 16)} height={size} aria-hidden={true} className="logo">
-    <path d="M 0 16 L 0 0 L 3 0 L 3 10 L 8 10 L 8 0 L 11 0 L 11 12 L 3 12 L 3 16 L 0 16 Z" fill={color} />
+const Logo = ({ theme, size }) => (
+  <svg viewBox="0 0 11 16" width={Math.round(size * 11 / 16)} height={size} aria-hidden={true} className={`logo logo-${theme}`}>
+    <path d="M 0 16 L 0 0 L 3 0 L 3 10 L 8 10 L 8 0 L 11 0 L 11 12 L 3 12 L 3 16 L 0 16 Z" />
   </svg>
 );
 
