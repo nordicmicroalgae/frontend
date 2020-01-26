f5f0ec167b56861999109f14000c4855edb9b08c
Andreas Loo <andreas.loo@smhi.se>
2020-01-26 14:35:13 +0100

    Use a default value of "introduction" when fetching page from backend

--- src/containers/PageContainer.js
+++ src/containers/PageContainer.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import Page from '../components/Page';
 
 
-function readPage(page) {
+function readPage(page = 'introduction') {
   return fetch(`/api/page/${page}/`)
     .then(response => {
       if (!response.ok) {
