7dfe82155d5db8fc9a87a08a58289d61d430aeb4
Andreas Loo <andreas.loo@smhi.se>
2020-02-12 15:02:32 +0100

    Add PageNotFound error class to pages API

--- src/api/pages.js
+++ src/api/pages.js
@@ -1,8 +1,25 @@
 
+export class PageNotFound extends Error {
+
+  constructor(...args) {
+    super(...args);
+
+    if (Error.captureStackTrace) {
+      Error.captureStackTrace(this, PageNotFound);
+    }
+
+    this.name = this.constructor.name;
+  }
+
+}
+
 export async function getPage(pageSlug) {
   const response = await fetch(`/api/page/${pageSlug}/`);
 
   if (!response.ok) {
+    if (response.status === 404) {
+      throw new PageNotFound(`Could not find page: ${pageSlug}`);
+    }
     throw new Error(`Could not fetch page: "${pageSlug}"`);
   }
 
