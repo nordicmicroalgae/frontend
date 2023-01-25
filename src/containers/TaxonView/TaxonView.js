b77f5d2a82ae7654d9e4f88eb66d988691a27a89
Andreas Loo <andreas@gaup.io>
2023-01-25 13:25:17 +0100

    Add tree navigation in TaxonView
    Features includes:
    
    - Expanding/collapsing tree nodes
    - Linking to other taxa in tree
    - Responsive design:
      - Navigation is contained within a sidebar for larger screens
      - Navigation is contained within an overlay triggered by a button
        for smaller screens

--- src/containers/TaxonView/TaxonView.js
+++ src/containers/TaxonView/TaxonView.js
@@ -1,17 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
 import Authority from '../../components/Authority';
 import ScientificName from '../../components/ScientificName';
+import Tree from '../../components/Taxonomy/Tree';
 import { useGetAllTaxaQuery, selectById } from '../../slices/taxa';
 
 
 const TaxonView = ({ match }) => {
+  const [ taxonomyIsExpanded, setTaxonomyIsExpanded ] = useState(false);
+
+  useEffect(() => {
+    if (taxonomyIsExpanded) {
+      document.body.classList.add('has-expanded-taxonomy');
+    } else {
+      document.body.classList.remove('has-expanded-taxonomy');
+    }
+  }, [ taxonomyIsExpanded ]);
+
   const query = useGetAllTaxaQuery();
 
   const taxon = useSelector(
     state => selectById(state, match.params.slug)
   );
+
+  const getTaxonKey = ({ slug }) => slug;
+
+  const handleClickToggleTaxonomy = _ev => {
+    setTaxonomyIsExpanded(!taxonomyIsExpanded);
+  };
 
   if (query.isLoading) {
     return <p>Loading...</p>;
@@ -32,6 +50,34 @@ const TaxonView = ({ match }) => {
           </>
         )}
       </h1>
+      <button
+        type="button"
+        className="taxonomy-toggle"
+        onClick={handleClickToggleTaxonomy}
+        aria-controls="taxonomy-navigation"
+        aria-expanded={taxonomyIsExpanded}
+      >
+        <span className="taxonomy-toggle-bar" />
+        <span className="taxonomy-toggle-bar" />
+        <span className="taxonomy-toggle-bar" />
+      </button>
+      <div id="taxonomy-navigation" className="taxon-view-taxonomy">
+        <h2 className="taxon-view-taxonomy-heading">
+          Taxonomy
+        </h2>
+        <Tree
+          data={query.data.entities}
+          getTaxonKey={getTaxonKey}
+          initialPath={
+            taxon.classification.map(getTaxonKey)
+          }
+          selected={getTaxonKey(taxon)}
+          Link={Link}
+          getLinkProps={({ slug }) => ({
+            to: `/taxon/${slug}`
+          })}
+        />
+      </div>
     </section>
   );
 };
--- src/containers/TaxonView/index.js
+++ src/containers/TaxonView/index.js
@@ -1,1 +1,2 @@
+import './TaxonView.scss';
 export { default } from './TaxonView.js';
