dbd5a044082219cba2823c12cf5d6b313eacff4a
Andreas Loo <andreas@gaup.io>
2023-03-15 12:26:44 +0100

    Improve Taxon View
    This commit adds some improvements on changes in state and on how
    loading and error states are being presented to the user in
    the Taxon View.
    
    Features includes:
    - Show placeholder while taxonomical data is being fetched
    - Show a friendly message if requested taxon does not exist
    - Scroll to top whenever the taxon identifier in the URL changes

--- src/containers/TaxonView/TaxonView.js
+++ src/containers/TaxonView/TaxonView.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
 import Authority from 'Components/Authority';
 import ScientificName from 'Components/ScientificName';
+import Placeholder from 'Components/Placeholder';
 import { useGetAllTaxaQuery, selectById } from 'Slices/taxa';
 import Taxonomy from './Taxonomy';
 import MediaView from 'Containers/MediaView';
@@ -13,44 +14,74 @@ import Facts from './Facts';
 const TaxonView = () => {
   const params = useParams();
 
-  const query = useGetAllTaxaQuery();
+  const { isFetching } = useGetAllTaxaQuery();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [params.slug]);
 
   const taxon = useSelector(
     state => selectById(state, params.slug)
   );
 
-  if (query.isLoading) {
-    return <p>Loading...</p>;
-  }
+  const isMissing = (
+    isFetching === false && taxon == null
+  );
 
   return (
-    <section className="taxon-view">
-      <h1>
-        <ScientificName>
-          {taxon.scientificName}
-        </ScientificName>
-        {taxon.authority && (
-          <>
-            {' '}
-            <Authority>
-              {taxon.authority}
-            </Authority>
-          </>
-        )}
-      </h1>
-      <Taxonomy taxon={params.slug} />
-      <div className="taxon-view-media">
-        <MediaView query={{taxon: taxon.slug}}>
-          <MediaView.Details />
-          <MediaView.Thumbnails fieldList={[]} />
-        </MediaView>
-      </div>
-      <div className="taxon-view-facts">
-        <Facts taxon={taxon.slug}>
-          <Facts.Biovolumes />
-        </Facts>
-      </div>
-    </section>
+    isFetching ? (
+      <section className="taxon-view is-loading">
+        <h1><Placeholder /></h1>
+        <Taxonomy taxon={params.slug} />
+      </section>
+    ) :
+    isMissing ? (
+      <section className="taxon-view is-missing">
+        <h1>Taxon not found</h1>
+        <p>
+          Unfortunately, the taxon you requested does
+          not seem to currently be in our database.
+        </p>
+        <p>
+          If you think that this is a misstake, please help us
+          to correct that by sending an email to our administrators.
+        </p>
+        <p>
+          You may also try to find the taxon you were looking for by
+          either using the taxonomy tree on this page or by using
+          the search field in the top navigation.
+        </p>
+        <Taxonomy taxon={params.slug} />
+      </section>
+    ) : (
+      <section className="taxon-view">
+        <h1>
+          <ScientificName>
+            {taxon.scientificName}
+          </ScientificName>
+          {taxon.authority && (
+            <>
+              {' '}
+              <Authority>
+                {taxon.authority}
+              </Authority>
+            </>
+          )}
+        </h1>
+        <Taxonomy taxon={params.slug} />
+        <div className="taxon-view-media">
+          <MediaView query={{taxon: taxon.slug}}>
+            <MediaView.Details />
+            <MediaView.Thumbnails fieldList={[]} />
+          </MediaView>
+        </div>
+        <div className="taxon-view-facts">
+          <Facts taxon={taxon.slug}>
+            <Facts.Biovolumes />
+          </Facts>
+        </div>
+      </section>
+    )
   );
 };
 
