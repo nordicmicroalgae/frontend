8dc17b8c943a7090296085b628ce0c095ff14209
Andreas Loo <andreas@gaup.io>
2024-02-20 23:34:37 +0100

    Do not replace hypens in getKey

--- src/utils/getKey.js
+++ src/utils/getKey.js
@@ -2,7 +2,7 @@ export default function getKey(...args) {
   const parts = args.map(value => {
     value = value.toString();
     value = value.toLowerCase();
-    value = value.replace(/[^a-z0-9]/g, '');
+    value = value.replace(/[^a-z0-9\-]/g, '');
     value = value.substring(0, 32);
     return value;
   });
