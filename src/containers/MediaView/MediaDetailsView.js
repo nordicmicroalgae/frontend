bba0bcf19ddc992f7bd963f5ae4274e31c7ebeb6
Andreas Loo <andreas@gaup.io>
2024-02-22 13:00:24 +0100

    Minor adjustments in MediaDetailsView
    - Remove Frame component
    - Use `100%` of container as width for image

--- src/containers/MediaView/MediaDetailsView.js
+++ src/containers/MediaView/MediaDetailsView.js
@@ -4,7 +4,6 @@ import PropTypes from 'prop-types';
 import { useMediaQuery } from './media-context';
 import { getAttributeList } from './field-utils';
 import Attributes from 'Components/Attributes';
-import Frame from 'Components/Media/Frame';
 import Picture from 'Components/Media/Picture';
 import { ChevronDownIcon, ChevronUpIcon } from 'Components/Icons';
 
@@ -31,14 +30,12 @@ const MediaDetailsView = ({ expandable }) => {
 
   return (selectedMedia && (
     <div className="media-details-view">
-      <Frame>
-        <Picture
-          src={selectedMedia.renditions.l.url}
-          backgroundColor="transparent"
-          backgroundImage={selectedMedia.renditions.p.url}
-          width={1024}
-        />
-      </Frame>
+      <Picture
+        src={selectedMedia.renditions.l.url}
+        backgroundColor="transparent"
+        backgroundImage={selectedMedia.renditions.p.url}
+        width={'100%'}
+      />
       <div className={
         expandable ? (
           `media-details ${isExpanded ? 'expanded' : 'collapsed'}`
