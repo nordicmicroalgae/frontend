3a75ad440d990c9c0d491f1e3c2612068993dc48
Andreas Loo <andreas@gaup.io>
2024-03-11 19:19:20 +0100

    Improve statistics component
    Features includes:
    - Locale formatted numbers
    - Linked pieces

--- src/containers/Home/Statistics/Statistics.js
+++ src/containers/Home/Statistics/Statistics.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
 
 import { useGetStatisticsQuery } from 'Slices/statistics';
 import Placeholder from 'Components/Placeholder';
@@ -11,6 +13,13 @@ const Fields = new Map([
   ['taxa', 'Taxa'],
   ['images', 'Images'],
   ['contributors', 'Contributors'],
+]);
+
+const Links = new Map([
+  ['species', '/checklists-and-biovolumes/'],
+  ['taxa', '/taxon/'],
+  ['images', '/gallery/'],
+  ['contributors', '/hall-of-fame/'],
 ]);
 
 const Statistics = () => {
@@ -36,13 +45,15 @@ const Statistics = () => {
         <h2>Statistics</h2>
         <dl>
         {[...Fields.entries()].map(([key, label]) => (
-          <React.Fragment key={getKey('statistics', key)}>
+          <Link
+            to={Links.get(key)}
+            key={getKey('statistics', key)}>
             <dt>
               {Graphic[label] && Graphic[label]()}
               {label}
             </dt>
-            <dd>{currentData[key]}</dd>
-          </React.Fragment>
+            <dd>{currentData[key].toLocaleString()}</dd>
+          </Link>
         ))}
         </dl>
       </section>
