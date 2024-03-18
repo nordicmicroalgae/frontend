a0225db4fc2bacd645c78955b972b8fc99ca0374
Andreas Loo <andreas@gaup.io>
2024-03-18 08:36:48 +0100

    Minor logo fixes

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -15,7 +15,7 @@ const defaultProps = {
 const Logo = ({ theme, size }) => (
   <svg viewBox="0 0 11 16" width={Math.round(size * 11 / 16)} height={size} aria-hidden={true} className={`logo logo-${theme}`}>
     <path className="logo-left" d="M 0 0 L 0 16 L 3 16 L 3 12 L 5.5 13 L 5.5 10 L 3 9 L 3 0 L 0 0 Z" />
-    <path className="logo-right" d="M 8 0 L 11 0 L 11 12 L 5.5 13 L 5.5 10 L 8 9" />
+    <path className="logo-right" d="M 8 0 L 11 0 L 11 11 L 5.5 13 L 5.5 10 L 8 9" />
   </svg>
 );
 
