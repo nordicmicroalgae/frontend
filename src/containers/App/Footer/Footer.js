d3176f5fba584ebd0518bda894a009ef37c7ad14
Andreas Loo <andreas@gaup.io>
2024-03-19 23:11:37 +0100

    Update footer with links and logos
    - Add background color for footer
    - Add logos and links for funding partners

--- src/containers/App/Footer/Footer.js
+++ src/containers/App/Footer/Footer.js
@@ -2,12 +2,28 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 
 
+import SBDILogo from '../../../assets/sbdi.png';
+import SMHILogo from '../../../assets/smhi.png';
+
+
 const Footer = () => (
   <footer className="footer" role="contentinfo">
-    <p>
-      This service is a part of the Swedish Biodiversity Data Infrastructure,
-      funded by SMHI and the Swedish Research Council through Grant No 2019-00242.
-    </p>
+    <div className="funding">
+      <p className="funding-info">
+        This service is a part of the <a href="https://biodiversitydata.se/">Swedish Biodiversity Data Infrastructure</a>,
+        funded by <a href="https://smhi.se/">SMHI</a> and the Swedish Research Council through Grant No 2019-00242.
+      </p>
+      <div className="funding-logo">
+        <a href="https://smhi.se/">
+          <img src={SMHILogo} alt="SMHI" />
+        </a>
+      </div>
+      <div className="funding-logo">
+        <a href="https://biodiversitydata.se/">
+          <img src={SBDILogo} alt="SBDI" />
+        </a>
+      </div>
+    </div>
     <nav role="navigation">
       <ul>
         <li>
--- webpack.config.js
+++ webpack.config.js
@@ -85,7 +85,11 @@ module.exports = {
             }
           }
         ]
-      }
-    ]
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
   },
 };
