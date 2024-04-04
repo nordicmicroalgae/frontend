86cd6e74b92bc67b4fbd9dfb6998034e6f811eeb
Andreas Loo <andreas@gaup.io>
2024-04-04 15:03:41 +0200

    Use initial expanded Taxonomy when no sel. taxon
    Makes the "Taxon tree" view more approachable on
    small devices.

--- src/containers/TaxonView/Taxonomy/Taxonomy.js
+++ src/containers/TaxonView/Taxonomy/Taxonomy.js
@@ -24,7 +24,7 @@ const Taxonomy = ({ taxon }) => {
     }
   }, [ navigationIsExpanded ]);
 
-  useEffect(() => setNavigationIsExpanded(false), [taxon]);
+  useEffect(() => setNavigationIsExpanded(taxon == null), [taxon]);
 
   const query = useGetAllTaxaQuery();
 
