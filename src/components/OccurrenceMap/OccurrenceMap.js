79aeedb6324fc4790f0291108d7709d09d38e903
Jonatan Skogsfors <jonatan.skogsfors@smhi.se>
2025-10-22 14:29:58 +0200

    Load occurrence for all synonyms
    Including controls for toggling which synonyms should be shown.
    
    Fixes #6

--- src/components/OccurrenceMap/OccurrenceMap.js
+++ src/components/OccurrenceMap/OccurrenceMap.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef } from 'react';
 import PropTypes from 'prop-types';
-import Map from 'ol/Map';
+import OLMap from 'ol/Map';
 import TileImage from 'ol/source/TileImage';
 import TileLayer from 'ol/layer/Tile';
 import View from 'ol/View';
@@ -12,6 +12,7 @@ import { getCenter } from 'ol/extent';
 const propTypes = {
   width: PropTypes.string,
   height: PropTypes.string,
+  externalIds: PropTypes.arrayOf(PropTypes.string),
   getAttributions: PropTypes.func,
   getViewExtent: PropTypes.func,
   getViewOptions: PropTypes.func,
@@ -23,6 +24,7 @@ const propTypes = {
 const defaultProps = {
   width: '100%',
   height: '50vh',
+  externalIds: [],
   getAttributions: () => [],
   getViewOptions: () => ({
     center: fromLonLat([8, 60]),
@@ -41,7 +43,7 @@ const defaultProps = {
 };
 
 const OccurrenceMap = ({
-  externalId,
+  externalIds,
   width,
   height,
   getAttributions,
@@ -53,13 +55,14 @@ const OccurrenceMap = ({
 }) => {
   const elRef = useRef();
   const olRef = useRef();
+  const layersRef = useRef(new globalThis.Map());
 
   useEffect(() => {
     const viewExtent = getViewExtent
       ? getViewExtent()
       : undefined;
 
-    const map = new Map({
+    const map = new OLMap({
       target: elRef.current,
       layers: [
         new TileLayer({
@@ -80,29 +83,48 @@ const OccurrenceMap = ({
     });
 
     olRef.current = map;
+
+    return () => {
+      const cached = layersRef.current;
+      if (map && cached) {
+        cached.forEach(layer => {
+          map.removeLayer(layer);
+        });
+        cached.clear();
+      }
+    };
   }, []);
 
   useEffect(() => {
     const map = olRef.current;
+    if (!map) return;
 
-    let layer;
+    const nextIds = new Set(externalIds || []);
+    const cachedLayers = layersRef.current;
 
-    if (map && externalId) {
-      layer = new TileLayer({
-        source: new TileImage({
-          ...getTileOptions(),
-          url: getOccurrenceTileUrl(externalId),
-        }),
-      });
-      map.addLayer(layer);
-    }
-
-    return () => {
-      if (map && layer) {
-        map.removeLayer(layer);
+    cachedLayers.forEach((layer, id) => {
+      if (!nextIds.has(id)) {
+        layer.setVisible(false);
       }
-    }
-  }, [externalId]);
+    });
+
+    nextIds.forEach(id => {
+      const existing = cachedLayers.get(id);
+      if (existing) {
+        existing.setVisible(true);
+      } else {
+        const layer = new TileLayer({
+          source: new TileImage({
+            ...getTileOptions(),
+            url: getOccurrenceTileUrl(id),
+          }),
+          visible: true,
+        });
+        map.addLayer(layer);
+        cachedLayers.set(id, layer);
+      }
+    });
+  }, [externalIds]);
 
   return (
     <div
